4901b5379cc85b3d3bb70aec618e8325a27f373a
yangsonglin <yangsonglin@tuyabeat.com>
2017-09-06 16:47:55 +0800

    日志

--- src/main/java/com/tuya/heipa/SearchServerApplication.java
+++ src/main/java/com/tuya/heipa/SearchServerApplication.java
@@ -6,13 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
+@Slf4j
 public class SearchServerApplication {
-
 	public static void main(String[] args) {
+		log.info("start");
 		SpringApplication.run(SearchServerApplication.class, args);
 	}
 }
