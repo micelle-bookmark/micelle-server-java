89ea0a52d44f7fe265594f31ccff62fe76e72e6b
lsytj0413 <511121939@qq.com>
2017-09-12 10:29:51 +0800

    bookmark package

---
+++ src/main/java/xin/soren/micelle/domain/mapper/bookmark/BookmarkMapper.java
@@ -0,0 +1,8 @@
+package xin.soren.micelle.domain.mapper.bookmark;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BookmarkMapper {
+
+}
---
+++ src/main/java/xin/soren/micelle/domain/model/bookmark/BookmarkEntity.java
@@ -0,0 +1,8 @@
+package xin.soren.micelle.domain.model.bookmark;
+
+import lombok.Data;
+
+@Data
+public class BookmarkEntity {
+	public Long id;
+}
