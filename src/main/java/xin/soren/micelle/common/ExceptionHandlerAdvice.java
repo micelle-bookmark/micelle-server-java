edb0d576c92334bad0793572129e5b1af9616aae
lsytj0413 <511121939@qq.com>
2017-09-13 20:43:50 +0800

    ExceptionHandlerAdvice

---
+++ src/main/java/xin/soren/micelle/common/ExceptionHandlerAdvice.java
@@ -0,0 +1,67 @@
+package xin.soren.micelle.common;
+
+import java.text.MessageFormat;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import lombok.extern.slf4j.Slf4j;
+import xin.soren.micelle.common.api.ApiResponseError;
+import xin.soren.micelle.exception.ExceptionBase;
+import xin.soren.micelle.exception.ExceptionCodeConst;
+
+/**
+ * 
+ * @Description: 异常处理
+ * @author soren
+ * @date 2017年9月13日 下午8:43:21
+ *
+ */
+@Component
+@Slf4j
+public class ExceptionHandlerAdvice {
+
+	@ExceptionHandler(value = Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ResponseBody
+	public Object handleUnknownException(Exception exception) {
+		log.error(MessageFormat.format("[未知错误] {0}", ExceptionUtils.getStackTrace(exception)));
+
+		return new ApiResponseError(ExceptionCodeConst.UNKNOWN_ERROR, "未知错误");
+	}
+
+	@ExceptionHandler(value = ExceptionBase.class)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
+	public Object handleExceptionBase(ExceptionBase exceptionBase) {
+		log.error(MessageFormat.format("{0}", ExceptionUtils.getStackTrace(exceptionBase)));
+
+		return new ApiResponseError(exceptionBase.getErrorCode(), exceptionBase.getErrorMsg());
+	}
+
+	// @ExceptionHandler(value = ForbiddenException.class)
+	// @ResponseBody
+	// public Object handleExceptionBase(ForbiddenException exceptionBase,
+	// HttpServletResponse rsp) {
+	// log.error(MessageFormat.format("{0}",
+	// ExceptionUtils.getStackTrace(exceptionBase)));
+	//
+	// rsp.setStatus(HttpStatus.FORBIDDEN.value());
+	// return new ApiErrorResponse(exceptionBase.getErrorCode(),
+	// exceptionBase.getErrorMsg());
+	// }
+	//
+	// @ExceptionHandler(value = DataAccessException.class)
+	// @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	// @ResponseBody
+	// public Object handleExcetptionBase(DataAccessException exception) {
+	// log.error(MessageFormat.format("[数据库错误] {0}",
+	// ExceptionUtils.getStackTrace(exception)));
+	//
+	// return new ApiErrorResponse(ExceptionCodeConst.S_200102, "数据库错误");
+	// }
+}
