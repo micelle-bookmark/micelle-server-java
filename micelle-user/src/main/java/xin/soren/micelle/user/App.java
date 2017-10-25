80471020e88baeb08f19fe3fa82d170e66786535
lsytj0413 <511121939@qq.com>
2017-10-25 10:35:39 +0800

    rename

--- micelle-user/src/main/java/xin/soren/micelle/user/App.java
+++ micelle-user/src/main/java/xin/soren/micelle/user/App.java
@@ -1,4 +1,4 @@
-package xin.soren.user;
+package xin.soren.micelle.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
