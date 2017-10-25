1bdfc70ffb6dbf0c8d155cb1551218ec5df086ae
lsytj0413 <511121939@qq.com>
2017-10-25 10:27:11 +0800

    module micelle-user

---
+++ micelle-user/src/main/java/xin/soren/user/App.java
@@ -0,0 +1,17 @@
+package xin.soren.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
