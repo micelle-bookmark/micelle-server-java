203e678e92e48ac1416f462ec268ac85383f5699
lsytj0413 <511121939@qq.com>
2017-11-03 21:33:57 +0800

    api

--- micelle-api/src/main/java/xin/soren/micelle/controller/bookmark/BookmarkController.java
+++ micelle-api/src/main/java/xin/soren/micelle/controller/bookmark/BookmarkController.java
@@ -1,5 +1,6 @@
 package xin.soren.micelle.controller.bookmark;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,61 @@ import xin.soren.micelle.controller.AuthTokenHelper;
 @Slf4j
 public class BookmarkController {
 
+	/**
+	 * 
+	 * @Description: 获取当前用户的所有书签
+	 * @return
+	 * @Throws
+	 * @date 2017年11月3日 下午9:27:20
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	@Api
 	public Object getUserBookmarkList() {
 		AuthSubject subject = AuthTokenHelper.getAuthSubject();
-		log.info("修改当前用户[{}]书签列表", subject.userId);
+		log.info("获取当前用户[{}]书签列表", subject.userId);
 
+		return null;
+	}
+
+	/**
+	 * 
+	 * @Description: 通过书签 ID 获取书签详细信息
+	 * @param bookmarkId
+	 * @return
+	 * @Throws
+	 * @date 2017年11月3日 下午9:27:33
+	 */
+	@RequestMapping(value = "/{bookmarkId}", method = RequestMethod.GET)
+	@Api
+	public Object getUserBookmarkById(@PathVariable("bookmarkId") Long bookmarkId) {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @Description: 通过书签 ID 获取子书签
+	 * @param bookmarkId
+	 * @return
+	 * @Throws
+	 * @date 2017年11月3日 下午9:30:32
+	 */
+	@RequestMapping(value = "/{bookmarkId}/children", method = RequestMethod.GET)
+	@Api
+	public Object getUserBookmarkByParentId(@PathVariable("bookmarkId") Long bookmarkId) {
+		return null;
+	}
+
+	/**
+	 * 
+	 * @Description: 通过书签 ID 删除用户书签
+	 * @param bookmarkId
+	 * @return
+	 * @Throws
+	 * @date 2017年11月3日 下午9:28:18
+	 */
+	@RequestMapping(value = "/{bookmarkId}", method = RequestMethod.DELETE)
+	@Api
+	public Object deleteUserBookmark(@PathVariable("bookmarkId") Long bookmarkId) {
 		return null;
 	}
 
